0b8b04938fa169f39fdba73c0915db997862447f
Hernan Mauriccio Gaston Berrospi Reyes <mauriccioreyes@gmail.com>
2024-10-21 22:42:04 -0500

    feat(backend) implementacion de lista de usuario junto a lista de servicio

--- src/app/lista-users/lista-users.component.ts
+++ src/app/lista-users/lista-users.component.ts
@@ -5,58 +5,68 @@ import { User } from '../user/user';
 import { UserService } from '../user/user.service';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { ServiceService } from '../service/service.service';
+import { Service } from '../service/service';
 
 @Component({
   selector: 'app-lista-users',
   standalone: true,
   imports: [CommonModule, FormsModule], // Agrega FormsModule aquí
   templateUrl: './lista-users.component.html',
-  styleUrls: ['./lista-users.component.css']
+  styleUrls: ['./lista-users.component.css'],
 })
 export class ListaUsersComponent implements OnInit {
   users: User[] = [];
+  services: Service[] = [];
   searchTerm: string = ''; // Usar una sola variable para la búsqueda
+  searchTerm1: string = ''; // Usar una sola variable para la búsqueda
 
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(
+    private userService: UserService,
+    private serviceService: ServiceService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.userService.obtenerListaDeUsuarios().subscribe(data => {
-      this.users = data;
-    });
+    this.obtenerService();
+    this.obtenerUser();
   }
 
-  actualizarUser(id: number){
-    this.router.navigate(["actualizar-user", id]);  // Navegación
+  actualizarUser(id: number) {
+    this.router.navigate(['actualizar-user', id]); // Navegación
   }
 
   eliminarUser(id: number): void {
     Swal.fire({
       title: '¿Estás seguro?',
-      text: "Confirma si deseas eliminar al usuario",
+      text: 'Confirma si deseas eliminar al usuario',
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
       confirmButtonText: 'Sí, elimínalo',
       cancelButtonText: 'No, cancelar',
-      buttonsStyling: true
+      buttonsStyling: true,
     }).then((result) => {
       if (result.isConfirmed) {
-        this.userService.eliminarUsuario(id).subscribe(dato => {
-          console.log(dato);
-          this.ngOnInit();  // Recarga la lista de usuarios
-          Swal.fire(
-            'Usuario eliminado',
-            'El usuario ha sido eliminado con éxito',
-            'success'
-          );
-        }, error => {
-          Swal.fire(
-            'Error',
-            'Hubo un error al intentar eliminar el usuario',
-            'error'
-          );
-        });
+        this.userService.eliminarUsuario(id).subscribe(
+          (dato) => {
+            console.log(dato);
+            this.ngOnInit(); // Recarga la lista de usuarios
+            Swal.fire(
+              'Usuario eliminado',
+              'El usuario ha sido eliminado con éxito',
+              'success'
+            );
+          },
+          (error) => {
+            Swal.fire(
+              'Error',
+              'Hubo un error al intentar eliminar el usuario',
+              'error'
+            );
+          }
+        );
       }
     });
   }
@@ -65,64 +75,131 @@ export class ListaUsersComponent implements OnInit {
     this.router.navigate(['detalle-user', id]);
   }
 
+  private obtenerUser(): void {
+    this.userService.obtenerListaDeUsuarios().subscribe((dato) => {
+      this.users = dato;
+    });
+  }
 
   buscarUsuarioporNombreoNro(): void {
     if (this.searchTerm.trim() !== '') {
-      const isNumber = /^\d+$/.test(this.searchTerm); // Verifica si el término es un número
-  
+      const isNumber = /^\d+$/.test(this.searchTerm); // Verifica si es un número
+
       if (isNumber) {
         // Si es un número, buscar por número de documento
-        this.userService.buscarUsuarioPorNombreONumberDoc(undefined, +this.searchTerm).subscribe(
-          (data) => {
-            if (data && data.length > 0) {
-              this.users = data; // Actualizar la lista de usuarios si hay resultados
+        this.userService
+          .buscarUsuarioPorNombreONumberDoc(undefined, +this.searchTerm)
+          .subscribe(
+            (data) => {
+              if (data && data.length > 0) {
+                this.users = data; // Actualizar la lista de usuarios si hay resultados
+              }
+            },
+            (error) => {
+              console.error('Error al buscar usuario:', error);
+              Swal.fire(
+                'No se encontró coincidencia',
+                `El usuario con número de documento "${this.searchTerm}" no fue encontrado`,
+                'error'
+              );
+              this.obtenerUser(); // Recargar la lista completa de usuarios en caso de error
             }
-          },
-          (error) => {
-            console.error('Error al buscar usuario:', error);
-            Swal.fire(
-              'Error',
-              `El usuario con número de documento "${this.searchTerm}" no fue encontrado`,
-              'error'
-            );
-            this.users = []; // Limpiar la lista de usuarios en caso de error
-          }
-        );
+          );
       } else {
         // Si no es un número, buscar por nombre
-        this.userService.buscarUsuarioPorNombreONumberDoc(this.searchTerm, undefined).subscribe(
-          (data) => {
-            if (data && data.length > 0) {
-              this.users = data; // Actualizar la lista de usuarios si hay resultados
+        this.userService
+          .buscarUsuarioPorNombreONumberDoc(this.searchTerm, undefined)
+          .subscribe(
+            (data) => {
+              if (data && data.length > 0) {
+                this.users = data; // Actualizar la lista de usuarios si hay resultados
+              }
+            },
+            (error) => {
+              console.error('Error al buscar usuario:', error);
+              Swal.fire(
+                'No se encontró coincidencia',
+                `El usuario con nombre "${this.searchTerm}" no fue encontrado`,
+                'error'
+              );
+              this.obtenerUser(); // Recargar la lista completa de usuarios en caso de error
             }
-          },
-          (error) => {
-            console.error('Error al buscar usuario:', error);
-            Swal.fire(
-              'No se encontró coincidencia',
-              `El usuario con nombre "${this.searchTerm}" no fue encontrado`,
-              'warning'
-            );
-            this.users = []; // Limpiar la lista de usuarios en caso de error
-          }
-        );
+          );
       }
     } else {
       // Si el campo de búsqueda está vacío, recargar la lista completa de usuarios
-      this.userService.obtenerListaDeUsuarios().subscribe(
-        (data) => {
-          this.users = data;
-        },
-        (error) => {
-          console.error('Error al cargar la lista de usuarios:', error);
-          Swal.fire(
-            'Error',
-            'Ocurrió un error al cargar la lista de usuarios.',
-            'error'
-          );
-        }
-      );
+      this.obtenerUser();
     }
   }
-  
+
+  actualizarService(id: number) {
+    this.router.navigate(['actualizar-service', id]);
+  }
+
+  private obtenerService(): void {
+    this.serviceService.obtenerListaDeServicios().subscribe((dato) => {
+      this.services = dato;
+    });
+  }
+
+  eliminarService(id: number): void {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: 'Confirma si deseas eliminar al servicio',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, elimínalo',
+      cancelButtonText: 'No, cancelar',
+      buttonsStyling: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.serviceService.eliminarServicio(id).subscribe((dato) => {
+          console.log(dato);
+          this.obtenerService();
+          Swal.fire(
+            'Servicio eliminado',
+            'El servicio ha sido eliminado con éxito',
+            'success'
+          );
+        });
+      }
+    });
+  }
+
+  detalleService(id: number) {
+    this.router.navigate(['detalle-service', id]);
+  }
+
+  buscarServicePorNombre(): void {
+    const regex = /^[a-zA-Z\s]*$/;
+
+    if (!regex.test(this.searchTerm1)) {
+      Swal.fire(
+        'Entrada no válida',
+        'Por favor, ingresa solo letras en el campo de búsqueda',
+        'error'
+      );
+      return;
+    }
+
+    if (this.searchTerm1.trim() !== '') {
+      this.serviceService.obtenerServicioPorName(this.searchTerm1).subscribe(
+        (service) => {
+          this.services = [service];
+        },
+        (error) => {
+          Swal.fire(
+            'No se encontró coincidencia',
+            `El servicio con el nombre "${this.searchTerm1}" no fue encontrado`,
+            'error'
+          );
+          this.obtenerService();
+        }
+      );
+    } else {
+      this.obtenerService();
+    }
+  }
 }

ca9836a07e84c5c9280c918a15f50ee1ec85955d
VaniaR <vramosc2@upao.edu.pe>
2024-11-05 23:41:25 -0500

    feat(frontend): implementación de casillas de estado de éxito

--- src/app/attend-tc/attend-tc.component.ts
+++ src/app/attend-tc/attend-tc.component.ts
@@ -2,13 +2,16 @@ import {Component, Injectable, OnInit} from '@angular/core';
 import {ticketAttendedTcComponent} from "./ticket-attend-tc.component";
 import {NgForOf, NgIf} from "@angular/common";
 import {ServiceService} from "../service/service.service";
+import { nextQueueService } from "../service/nextQueue.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-attend-tc',
   standalone: true,
   imports: [
     NgIf,
-    NgForOf
+    NgForOf,
+    FormsModule
   ],
   templateUrl: './attend-tc.component.html',
   styleUrl: './attend-tc.component.css'
@@ -22,8 +25,9 @@ export class AttendTcComponent implements OnInit{
   ticketData: any;
   services: any[] = [];
   chosenServices: any[] = [];
-
-  constructor(private ticketService: ticketAttendedTcComponent, private serviceService: ServiceService) {}
+  successful: boolean = false;
+  notSuccessful: boolean = false;
+  constructor(private ticketService: ticketAttendedTcComponent, private serviceService: ServiceService, private nextQueueService: nextQueueService) {}
 
   ngOnInit(): void {
     this.ticketService.getTicketData().subscribe(data => {
@@ -35,5 +39,24 @@ export class AttendTcComponent implements OnInit{
     this.serviceService.obtenerListaDeServicios().subscribe((dato) => {
       this.services = dato;
     });
+  }
+  markAsSuccessful() {
+    if (!this.successful) {
+      this.nextQueueService.markAsSuccessful().subscribe(() => {
+        this.successful = true;
+        this.notSuccessful = false;
+        console.log("Ticket marcado como Satisfecho");
+      });
+    }
+  }
+
+  markAsNotSuccessful() {
+    if (!this.notSuccessful) {
+      this.nextQueueService.markAsNotSuccessful().subscribe(() => {
+        this.notSuccessful = true;
+        this.successful = false;
+        console.log("Ticket marcado como No Satisfecho");
+      });
+    }
   }
 }

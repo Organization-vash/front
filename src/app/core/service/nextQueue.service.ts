73f7bb34f6df647f5be31664a7532549946f6c4e
Sergio <svelasquezr1@upao.edu.pe>
2024-11-20 19:21:16 -0500

    feat(frontend) se agregó el hisotrial de códigos

--- src/app/core/service/nextQueue.service.ts
+++ src/app/core/service/nextQueue.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import { TicketHistory } from "../../shared/models/ticket-history.model";
 
 @Injectable({
   providedIn: 'root'
@@ -40,5 +41,9 @@ export class nextQueueService {
 
   finalizarAtencion(id: number): Observable<any> {
     return this.http.post<any>(`${this.baseURL}/${id}/finalize`, {});
+  }
+
+  getTodayTickets(moduleId: number): Observable<TicketHistory[]> {
+    return this.http.get<TicketHistory[]>(`${this.baseURL}/getHistory?moduleId=${moduleId}`);
   }
 }
